a27dc9727bf1be35e3327cdeb721307d34c95e24
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-10-02 15:20:51 +0200

    Formatting

--- xml_rpc_server/server.dart
+++ xml_rpc_server/server.dart
@@ -3,14 +3,16 @@ import 'package:xml_rpc/simple_server.dart' as server;
 main() async {
   const url = '127.0.0.1';
   const port = 8080;
-  final handler = server.XmlRpcHandler(methods: {
-    'sum': (params) {
-      var sum = 0;
-      params.forEach((value) => sum += value);
-      print(sum);
-      return 'Sum: $sum';
-    }
-  });
+  final handler = server.XmlRpcHandler(
+    methods: {
+      'sum': (params) {
+        var sum = 0;
+        params.forEach((value) => sum += value);
+        print(sum);
+        return 'Sum: $sum';
+      }
+    },
+  );
   final s = server.SimpleXmlRpcServer(host: url, port: port, handler: handler);
   await s.start();
   print('Starting server on port $port');
